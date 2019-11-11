77b38abeb34ded4b0d7024c60ccf155e134dea1b
Daniel Mendes Maack <danielmaack@cwi.com.br>
2019-11-11 16:18:39 -0300

    melhorando pagina de compartilhamento

--- ApiGiphy/api_giphy/lib/pages/ui/gif_page.dart
+++ ApiGiphy/api_giphy/lib/pages/ui/gif_page.dart
@@ -10,22 +10,49 @@ class GifPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_gifData["title"]),
         backgroundColor: Colors.black,
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.share),
-              onPressed: () {
-                Share.share(_gifData["images"]["fixed_height"]["url"]);
-              })
-        ],
+        title: Image.network(
+            "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
+        centerTitle: true,
       ),
       backgroundColor: Colors.black,
-      body: Row(
+      body: Column(
         children: <Widget>[
-          Expanded(child: Text(_gifData["title"])),
-          Expanded(
-              child: Image.network(_gifData["images"]["fixed_height"]["url"]))
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 90,
+                  child: Text(
+                    _gifData["title"],
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                Expanded(
+                  flex: 10,
+                  child: IconButton(
+                    color: Colors.blue,
+                    icon: Icon(Icons.share, color: Colors.white),
+                    onPressed: () {
+                      Share.share(_gifData["images"]["fixed_height"]["url"]);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 100,
+                child: Container(
+                  child:
+                      Image.network(_gifData["images"]["fixed_height"]["url"]),
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
